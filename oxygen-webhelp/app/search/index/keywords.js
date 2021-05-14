a0a94296fc1d91cdbce80a0000334bde0efe1172
dorota <d.cakar@vistula.edu.pl>
2021-05-14 17:05:20 +0100

    14.05

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,17 @@
+define(function() {var keywords=[{w:"Projekt",p:["p0"]},{w:"Komunikacja",p:["p0"]},{w:"Techniczna",p:["p0"]},{w:"Rekomendacje",p:["p1"]},{w:"Oferta",p:["p2"]},{w:"studi\u00F3w",p:["p2","p9"]},{w:"podyplomowych",p:["p2","p9"]},{w:"Rekrutacja",p:["p3"]},{w:"na",p:["p3"]},{w:"studia",p:["p3"]},{w:"podyplomowe",p:["p3"]},{w:"Op\u0142aty",p:["p4"]},{w:"Nauka",p:["p5"]},{w:"online",p:["p5"]},{w:"\u015Awiadectwo",p:["p6"]},{w:"Wzory",p:["p7"]},{w:"dokument\u00F3w",p:["p7"]},{w:"Regulaminy",p:["p8"]},{w:"i",p:["p8"]},{w:"akty",p:["p8"]},{w:"prawne",p:["p8"]},{w:"Kontakt",p:["p9"]},{w:"do",p:["p9"]},{w:"Dzia\u0142u",p:["p9"]},{w:"AFiB",p:["p9"]},{w:"Vistula",p:["p9"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3];
+ph["p2"]=[4, 5, 6];
+ph["p3"]=[7, 8, 9, 10];
+ph["p4"]=[11];
+ph["p5"]=[12, 13];
+ph["p6"]=[14];
+ph["p7"]=[15, 16];
+ph["p8"]=[17, 18, 19, 20];
+ph["p9"]=[21, 22, 23, 5, 6, 24, 25];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
